a911118492d29d5751ca3dce41223e5a85ddf628
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2009-09-17 14:34:32 -0300

    Binary must not print

--- perl/sha1.pl
+++ perl/sha1.pl
@@ -11,8 +11,8 @@ use Digest::SHA1  qw(sha1 sha1_hex sha1_base64);
 
 my $data = do { local $/; <$in> };
 
-print "SHA1           ", sha1($data), "\n";
-print "SHA1 hex       ", sha1_hex($data), "\n";
-print "SHA1 base64    ", sha1_base64($data), "\n";
+# print "SHA1         ", sha1($data)       , "\n"; Binary
+  print "SHA1 hex     ", sha1_hex($data)   , "\n";
+  print "SHA1 base64  ", sha1_base64($data), "\n";
 
 exit 0;
