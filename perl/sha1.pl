8e233ab89ecc3489ee8f420b3230f4c589892309
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2009-09-17 14:31:08 -0300

    Added.

---
+++ perl/sha1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+#
+# Usage: $0 file
+#
+# Marcus Vinicus Ferreira                   ferreira.mv[ at ]gmail.com
+#
+# 2009/09
+#
+
+use Digest::SHA1  qw(sha1 sha1_hex sha1_base64);
+
+my $data = do { local $/; <$in> };
+
+print "SHA1           ", sha1($data), "\n";
+print "SHA1 hex       ", sha1_hex($data), "\n";
+print "SHA1 base64    ", sha1_base64($data), "\n";
+
+exit 0;
