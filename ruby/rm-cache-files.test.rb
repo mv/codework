bbd50bea8574de09cf107cdf2ea580e88926ffea
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-26 15:38:51 -0200

    Reference...

---
+++ ruby/rm-cache-files.test.rb
@@ -0,0 +1,28 @@
+
+
+require 'test/unit'
+require 'empty'
+
+class EmptyTest < Test::Unit::TestCase
+
+    # pre
+    def setup
+    end
+
+    # post
+    def teardown
+    end
+
+    # tests
+    def test_usage
+    end
+
+    def test_rm
+    end
+
+    def test_keep
+    end
+
+    def test_list_verbose
+    end
+end
--- synergy/synergy-server.sh
+++ synergy/synergy-server.sh
@@ -18,14 +18,12 @@ fi
 ip0=$( ifconfig en0 | awk '/inet / {print $2}' )
 ip1=$( ifconfig en1 | awk '/inet / {print $2}' )
 
-case $ip0 in
-    '10.73.92.103')
-        /usr/local/bin/synergys -c ~/bin/synergy-server.conf -f --restart
-        ;;
-    *)
-        echo "ip0: $ip0"
-        ;;
-esac
+if [ -n $ip0 ]
+then
+    /usr/local/bin/synergys -c ~/bin/synergy-server.conf -f --restart
+else
+    echo "ip0: $ip0"
+fi
 
 case $ip1 in
     *)
