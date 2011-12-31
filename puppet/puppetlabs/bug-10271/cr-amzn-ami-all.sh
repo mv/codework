5a536004ed92a3a61b2d2e16e0fc81bd367bea44
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2011-12-31 17:23:18 -0200

    All tested.

--- puppet/puppetlabs/bug-10271/cr-amzn-ami-all.sh
+++ puppet/puppetlabs/bug-10271/cr-amzn-ami-all.sh
@@ -1,7 +1,11 @@
 
 ec2-create() {
-#   ec2-run-instances --region ${1} -z ${1}${2} -g ${3} -k ${4} -t ${5} ${6} \
-#   | gawk '{print strftime("%Y-%m-%d %H:%M:%S :", systime()), $0 }' >> $CREATED
+    echo "Creating ${1}${2}: $6"
+    ec2-run-instances --region ${1} -z ${1}${2} -g ${3} -k ${4} -t ${5} ${6} \
+    | tee -a $CREATED \
+    | gawk '{print strftime("%Y-%m-%d %H:%M:%S :", systime()), $0 }' >> $LOG
+#   echo "Sleeping...."
+#   sleep 45
 }
 
 CREATED="created.txt"
@@ -12,26 +16,33 @@ REGION="us-east-1"
  GROUP="server"
    KEY="marcus_anirul"
 
-# Current list
+# Init all files
 echo "## $(date)" > $CREATED
+> list-instance.txt
+> list-ip.txt
+> server.rb
+> terminate.sh
 
-ec2-create $REGION a $GROUP $KEY $TYPE ami-0af30663  # amzn-ami-0.9.7-beta.x86_64-ebs
-ec2-create $REGION b $GROUP $KEY $TYPE ami-38c33651  # amzn-ami-0.9.8-beta.x86_64-ebs
+# create
+ec2-create $REGION c $GROUP $KEY $TYPE ami-0af30663  # amzn-ami-0.9.7-beta.x86_64-ebs
+ec2-create $REGION c $GROUP $KEY $TYPE ami-38c33651  # amzn-ami-0.9.8-beta.x86_64-ebs
 ec2-create $REGION c $GROUP $KEY $TYPE ami-2272864b  # amzn-ami-0.9.9-beta.x86_64-ebs
-ec2-create $REGION a $GROUP $KEY $TYPE ami-74f0061d  # amzn-ami-2010.11.1-beta.x86_64-ebs
-ec2-create $REGION a $GROUP $KEY $TYPE ami-8e1fece7  # amzn-ami-2011.02.1.x86_64-ebs
-ec2-create $REGION b $GROUP $KEY $TYPE ami-7341831a  # amzn-ami-2011.09.1.x86_64-ebs
+ec2-create $REGION c $GROUP $KEY $TYPE ami-74f0061d  # amzn-ami-2010.11.1-beta.x86_64-ebs
+ec2-create $REGION c $GROUP $KEY $TYPE ami-8e1fece7  # amzn-ami-2011.02.1.x86_64-ebs
+ec2-create $REGION c $GROUP $KEY $TYPE ami-7341831a  # amzn-ami-2011.09.1.x86_64-ebs
 ec2-create $REGION c $GROUP $KEY $TYPE ami-1b814f72  # amzn-ami-2011.09.2.x86_64-ebs
 
-#ec2-describe-instances --region us-east-1 > created.txt
+## ec2-describe-instances --region us-east-1 | grep running > $CREATED
 
-awk '/^INSTANCE/ {print $4}' $CREATED                  > list-instance.txt
-awk '/^INSTANCE/ {print $6}' $CREATED | grep amazonaws > list-ip.txt
+# Support files
+awk '/INSTANCE/ {print $2}' $CREATED                  >> list-instance.txt
+awk '/INSTANCE/ {print $4}' $CREATED | grep amazonaws >> list-ip.txt
 
+# to be used by Capfile
 awk '{print "server \""$0"\" , :ec2"}' list-ip.txt | column -t> server.rb
 
-# Logging
-cat -n created.txt | column -t >> $LOG
-echo "Done."                   >> $LOG
+# to terminate it all
+awk '{print "ec2-terminate-instances --region REGION ", $0}' list-instance.txt > terminate.sh
+perl -pi -e "s/REGION/$REGION/" terminate.sh
 
 
---
+++ puppet/puppetlabs/bug-10271/terminate.sh
@@ -0,0 +1,7 @@
+ec2-terminate-instances --region us-east-1  i-16900474
+ec2-terminate-instances --region us-east-1  i-44910526
+ec2-terminate-instances --region us-east-1  i-52920630
+ec2-terminate-instances --region us-east-1  i-b89206da
+ec2-terminate-instances --region us-east-1  i-d69307b4
+ec2-terminate-instances --region us-east-1  i-c29c08a0
+ec2-terminate-instances --region us-east-1  i-129d0970
