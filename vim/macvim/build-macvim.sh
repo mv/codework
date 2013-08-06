63d7260e37232794f52a3e7e790bd73bfa7a4e00
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2013-08-05 23:12:10 -0300

    Overwritting brew

--- vim/macvim/build-macvim.sh
+++ vim/macvim/build-macvim.sh
@@ -6,10 +6,8 @@
 # 2009/09
 #
 
-DIR=/pub/src/macvim/
-SRC=/pub/src/macvim/git-macvim/src
 
-DEST=~/App/
+DEST=/usr/local/macvim
 
 # Ref:
 #     http://code.google.com/p/macvim/wiki/Building?tm=4
@@ -19,28 +17,41 @@ DEST=~/App/
 #     http://download.damieng.com/latest/EnvyCodeR
 #
 
-[ -d $SRC ] || {
-    printf "\nNot found src [$SRC]. Exitting...\n\n"
-    exit 1
-}
-
-cd $SRC
+cd ./src
+set -e
 
 make clean
-
+#
 PATH=/usr/bin:/bin ./configure \
-    --enable-gui=macvim \
-    --enable-perlinterp \
-    --enable-rubyinterp \
-    --disable-workshop  \
-    --disable-netbeans  \
-    --disable-netbeans_intg  \
-    --enable-cscope     \
-    2>&1 | tee configure.my.log
-    \
-    make            2>&1 | tee make.my.log          && \
-    \
-    cd MacVim && \
-    xcodebuild      2>&1 | tee xcodebuild.my.log    && \
-    open build/Release/MacVim.app
+    --with-features=huge    \
+    --enable-gui=macvim     \
+    --enable-perlinterp     \
+    --enable-rubyinterp     \
+    --enable-pythoninterp   \
+    --enable-luainterp      \
+    --enable-cscope         \
+    --disable-workshop      \
+    --disable-netbeans      \
+    --disable-netbeans_intg \
+         2>&1 | tee configure.my.log
+    make 2>&1 | tee make.my.log
+
+    cd MacVim && xcodebuild 2>&1 | tee xcodebuild.my.log
+
+    # destination
+    mkdir   -p  ${DEST}/bin
+
+    # .app
+    /bin/rm -rf ${DEST}/MacVim.app && /bin/cp -rp build/Release/MacVim.app ${DEST}/
+    sudo ln -nsf ${DEST}/MacVim.app /Applications/
+
+    # command line
+    pwd
+    /bin/cp -f MacVim/mvim ${DEST}/bin/
+    for f in gview gvim gvimdiff gvimex mview mvim mvimdiff mvimex vi view vim vimdiff vimex
+    do
+       ln -nsf ${DEST}/bin/mvim /usr/local/bin/${f}
+    done
+
+# vim:ft=sh:
 
