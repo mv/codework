c0544c4a5d1e56e539530a17626da2a644d3c0d6
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2014-01-07 17:02:58 -0200

    Whitespace.

--- bash/snippets/usage-getopt.sh
+++ bash/snippets/usage-getopt.sh
@@ -6,7 +6,7 @@ function usage() {
 
   Usage :  ${0##/*/} [options] [--]
 
-  Options: 
+  Options:
   -h|help       Display this message
   -v|version    Display script version
 
@@ -15,12 +15,12 @@ function usage() {
 }
 
 ### Handle command line arguments
-while getopts ":hv" opt
+while getopts ":hvf:" opt
 do
     case $opt in
 
+    f|file     )  file="$OPTARG"; ;;
     h|help     )  usage; exit 0   ;;
-
     v|version  )  echo "$0 -- Version $ScriptVersion"; exit 0   ;;
 
     \? )  echo -e "\n  Option does not exist : $OPTARG\n"
