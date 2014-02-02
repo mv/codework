d26cc1a16a46d84a0e754532951478e176f8effb
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2014-02-02 13:43:39 -0200

    Create a bootable pendrive.

---
+++ macos/macos-install-10.09-mavericks.sh
@@ -0,0 +1,7 @@
+
+sudo \
+    /Applications/Install\ OS\ X\ Mavericks.app/Contents/Resources/createinstallmedia \
+    --applicationpath /Applications/Install\ OS\ X\ Mavericks.app \
+    --volume /Volumes/Untitled                                    \
+    --nointeraction
+
