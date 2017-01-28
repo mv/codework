fa12275b374809afc39551c287e5940048258edc
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2017-01-28 17:24:43 -0200

    Align

--- kumon/cd1-kumon-rename-tracks.sh
+++ kumon/cd1-kumon-rename-tracks.sh
@@ -5,24 +5,26 @@
 # ferreira.mv@gmail.com
 # 2016-02
 
-/bin/mv '01 Track 01.mp3' '01.Folha.01-05.mp3'  2>/dev/null
-/bin/mv '02 Track 02.mp3' '02.Folha.06-10.mp3'  2>/dev/null
-/bin/mv '03 Track 03.mp3' '03.Folha.11-15.mp3'  2>/dev/null
-/bin/mv '04 Track 04.mp3' '04.Folha.16-20.mp3'  2>/dev/null
-/bin/mv '05 Track 05.mp3' '05.Folha.21-25.mp3'  2>/dev/null
-/bin/mv '06 Track 06.mp3' '06.Folha.26-30.mp3'  2>/dev/null
-/bin/mv '07 Track 07.mp3' '07.Folha.31-35.mp3'  2>/dev/null
-/bin/mv '08 Track 08.mp3' '08.Folha.36-40.mp3'  2>/dev/null
-/bin/mv '09 Track 09.mp3' '09.Folha.41-45.mp3'  2>/dev/null
-/bin/mv '10 Track 10.mp3' '10.Folha.46-50.mp3'  2>/dev/null
-/bin/mv '11 Track 11.mp3' '11.Folha.51-55.mp3'  2>/dev/null
-/bin/mv '12 Track 12.mp3' '12.Folha.56-60.mp3'  2>/dev/null
-/bin/mv '13 Track 13.mp3' '13.Folha.61-65.mp3'  2>/dev/null
-/bin/mv '14 Track 14.mp3' '14.Folha.66-70.mp3'  2>/dev/null
-/bin/mv '15 Track 15.mp3' '15.Folha.71-75.mp3'  2>/dev/null
-/bin/mv '16 Track 16.mp3' '16.Folha.76-80.mp3'  2>/dev/null
-/bin/mv '17 Track 17.mp3' '17.Folha.81-85.mp3'  2>/dev/null
-/bin/mv '18 Track 18.mp3' '18.Folha.86-90.mp3'  2>/dev/null
-/bin/mv '19 Track 19.mp3' '19.Folha.91-95.mp3'  2>/dev/null
-/bin/mv '20 Track 20.mp3' '20.Folha.96-100.mp3' 2>/dev/null
+/bin/mv '01 Track 01.mp3' '01.Folha.01-05.mp3'   2>/dev/null
+/bin/mv '02 Track 02.mp3' '02.Folha.06-10.mp3'   2>/dev/null
+/bin/mv '03 Track 03.mp3' '03.Folha.11-15.mp3'   2>/dev/null
+/bin/mv '04 Track 04.mp3' '04.Folha.16-20.mp3'   2>/dev/null
+/bin/mv '05 Track 05.mp3' '05.Folha.21-25.mp3'   2>/dev/null
+/bin/mv '06 Track 06.mp3' '06.Folha.26-30.mp3'   2>/dev/null
+/bin/mv '07 Track 07.mp3' '07.Folha.31-35.mp3'   2>/dev/null
+/bin/mv '08 Track 08.mp3' '08.Folha.36-40.mp3'   2>/dev/null
+/bin/mv '09 Track 09.mp3' '09.Folha.41-45.mp3'   2>/dev/null
+/bin/mv '10 Track 10.mp3' '10.Folha.46-50.mp3'   2>/dev/null
+/bin/mv '11 Track 11.mp3' '11.Folha.51-55.mp3'   2>/dev/null
+/bin/mv '12 Track 12.mp3' '12.Folha.56-60.mp3'   2>/dev/null
+/bin/mv '13 Track 13.mp3' '13.Folha.61-65.mp3'   2>/dev/null
+/bin/mv '14 Track 14.mp3' '14.Folha.66-70.mp3'   2>/dev/null
+/bin/mv '15 Track 15.mp3' '15.Folha.71-75.mp3'   2>/dev/null
+/bin/mv '16 Track 16.mp3' '16.Folha.76-80.mp3'   2>/dev/null
+/bin/mv '17 Track 17.mp3' '17.Folha.81-85.mp3'   2>/dev/null
+/bin/mv '18 Track 18.mp3' '18.Folha.86-90.mp3'   2>/dev/null
+/bin/mv '19 Track 19.mp3' '19.Folha.91-95.mp3'   2>/dev/null
+/bin/mv '20 Track 20.mp3' '20.Folha.96-100.mp3'  2>/dev/null
+/bin/mv '21 Track 21.mp3' '21.Folha.101-105.mp3' 2>/dev/null
+/bin/mv '22 Track 22.mp3' '22.Folha.106-110.mp3' 2>/dev/null
 
